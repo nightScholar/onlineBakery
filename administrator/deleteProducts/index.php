304f9f9da03ed9c5b3141db01b389969879ac2d8
Tyree <22990609+nightScholar@users.noreply.github.com>
2017-12-09 01:09:56 +0000

    removed product image field from DDL product table and added delete products index page for administrators

---
+++ administrator/deleteProducts/index.php
@@ -0,0 +1,34 @@
+<?php
+try
+{
+
+  $pdo = new PDO('mysql:host=localhost;dbname=bakery', 'tsolomonphillips', 'raidTombs2463%');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->exec('SET NAMES "utf8"');
+}
+catch (PDOException $e)
+{
+  $error = 'Unable to connect to the database server.';
+  include 'error.html.php';
+  exit();
+}
+
+try
+{
+
+  $sql = 'SELECT productDescription FROM product_t';
+  $result = $pdo->query($sql);
+}
+catch (PDOException $e)
+{
+  $error = 'Error fetching products names: ' . $e->getMessage();
+  include 'error.html.php';
+  exit();
+}
+
+while ($row = $result->fetch())
+{
+  $products[] = $row['productDescription'];
+}
+
+include 'view_products.html.php';
