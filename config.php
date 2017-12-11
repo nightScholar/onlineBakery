4907481405508631c9f7fec6db9f214344b12282
Fzuo <fzuo@luc.edu>
2017-12-10 21:41:01 -0600

    config.php added

--- config.php
+++ config.php
@@ -1,4 +1,12 @@
 <?php 
-	define('DB_Server', 'localhost:80');
-	
+	define('DB_SERVER', '127.0.0.1');
+	define('DB_USERNAME', 'xxx');
+	define('DB_PASSWORD', 'xxx');
+	define('DB_DATABASE', 'bakery');
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+	//Check connection
+	if(mysqli_connect_errno()){
+		echo "Failed to connect to MySQL database: ".mysqli_connect_error();
+	}
 ?>
